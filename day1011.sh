c2fa3f3edae2ab4f46b695243fdd47effe2a5643
MrunaliDeore <mrunalideoreohk1@gmail.com>
2022-06-13 18:08:00 +0530

    Updated file

--- day1011.sh
+++ day1011.sh
@@ -1,1 +1,22 @@
-Assignment 10&11
+a=10
+b=20
+c=30
+
+first=$(( $a + $b * $c))
+second=$(( $a * $b + $c))
+third=$(( $a + $b / $c))
+fourth=$(( $a % $b + $c))
+ 
+echo "First Answer is: $first"
+echo "Second Answer is: $second"
+echo "Third Answer is: $third"
+echo "Fourth Answer is: $fourth"
+
+
+
+OUTPUT:
+$ bash day1011.sh
+First Answer is: 610
+Second Answer is: 230
+Third Answer is: 10
+Fourth Answer is: 40
